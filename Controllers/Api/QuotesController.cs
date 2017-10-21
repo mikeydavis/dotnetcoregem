712f5e17d2f3de7ee80a43ebc490f5fe24187431
Mike Davis <mikedavis@Mikes-MacBook-Air.local>
2017-10-22 00:04:26 +0100

    quotes di removed api added 17

---
+++ Controllers/Api/QuotesController.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace aspnetcore
+{
+    
+    
+    [Route("api/[controller]")]
+    public class QuotesController : Controller
+    {
+        
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<Quotes1> Get()
+        {
+            var  quotes = new QuotesContext1().Quotes.ToList();
+            return quotes;
+        }
+
+        [HttpGet("{id}", Name = "GetQuotes")]
+        public IActionResult GetById(long id)
+        {
+            var item = new QuotesContext1().Quotes.ToList().FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+    }
+}
---
+++ Models/QuotesContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+//namespace Models
+//{
+    public class QuotesContext1 : DbContext
+    {
+        public DbSet<Quotes1> Quotes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(@"Server=midax-a-minjs-rds.cuxi7qfyfqir.us-west-2.rds.amazonaws.com;User Id=admin;Password=Aneeka97;Database=test");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Quotes1>(entity =>
+            {
+                entity.ToTable("quotes");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("int(11) unsigned");
+
+                entity.Property(e => e.Credit)
+                    .HasColumnName("credit")
+                    .HasColumnType("varchar(200)");
+
+                entity.Property(e => e.Quote)
+                    .HasColumnName("quote")
+                    .HasColumnType("varchar(200)");
+            });
+        }
+    }
+//}
