32edd1efa44a2444a30e720c3aed44ab43c3525d
Mike Davis <mikedavis@Mikes-MacBook-Air.local>
2017-11-12 08:30:47 +0000

    code clean up

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,21 +15,15 @@ namespace Controllers
         public HomeController(QuotesContext context){
             _context = context;
         }
-        public void GetAll()
-        {
-            QuotesContext qc = new QuotesContext();
-            //return quotes;
-        }
 
         [HttpGet]
         public JsonResult Get(string credit)
         {
-            return Json(new QuotesContext().Quotes.ToList());
+            return Json(_context.Quotes);
         }
         public async Task<IActionResult> Index()
         {
             var quotes = await _context.Quotes.ToListAsync();
-            //var  quotes = await new  QuotesContext1().Quotes.ToListAsync();
             return View(quotes);
         }
 
@@ -53,13 +47,9 @@ namespace Controllers
             {
                 return BadRequest();
             }
-            string quot = model.Quote;
-            var  context = new QuotesContext();
-            context.Add(model);
-            context.SaveChanges();
-            //return  CreatedAtRoute("GetQuotes", new { id = model.Id}, model);
-            //return View("",model.Quote);
-            var  quotes = new  QuotesContext().Quotes.ToList<Quotes>();
+            _context.Add(model);
+            _context.SaveChanges();
+            var quotes = _context.Quotes.ToList();
             return View("Index", quotes);
         }
 
@@ -68,43 +58,5 @@ namespace Controllers
         {
             return View();
         }
-    }
-
-
-    public class QuotesContext1 : DbContext
-    {
-        public DbSet<Quotes> Quotes { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseMySql(@"Server=midax-a-minjs-rds.cuxi7qfyfqir.us-west-2.rds.amazonaws.com;User Id=admin;Password=Aneeka97;Database=test");
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Quotes>(entity =>
-            {
-                entity.ToTable("quotes");
-
-                entity.Property(e => e.Id)
-                    .HasColumnName("id")
-                    .HasColumnType("int(11) unsigned");
-
-                entity.Property(e => e.Credit)
-                    .HasColumnName("credit")
-                    .HasColumnType("varchar(200)");
-
-                entity.Property(e => e.Quote)
-                    .HasColumnName("quote")
-                    .HasColumnType("varchar(200)");
-            });
-        }
-    }
-
-    public class Quotes1
-    {
-        public int Id { get; set; }
-        public string Credit { get; set; }
-        public string Quote { get; set; }
     }
 }
